11c9cd66315cadd34646abf8fe0c82b95e0b1361
Leonardo Isso <leoisso.work@gmail.com>
2018-10-25 22:38:43 -0300

    Add sass to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,17 +16,32 @@ module.exports = function(grunt) {
         dest: 'dist/',
       },
     },
+    sass: {                              
+      dist: {                            
+        options: {
+          style: 'expanded'
+        },
+        files: [{
+          expand: true,
+          cwd: 'src/sass',
+          src: ['skull.sass'],
+          dest: 'src/css/',
+          ext: '.css'
+        }]
+      }
+    },
     watch: {
       files: ['<%= copy.main.cwd %>'],
-      tasks: ['']
+      tasks: ['sass']
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build', ['clean', 'copy']);
+  grunt.registerTask('build', ['clean', 'sass', 'copy']);
 
 };
