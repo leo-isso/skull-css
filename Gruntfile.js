4bbeae98469227b5d63045e6fd3d2a17f27f2892
Leonardo Isso <leoisso.work@gmail.com>
2018-10-25 22:01:13 -0300

    Prepare Grunt copy and clean scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,19 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    clean: {
+      build: {
+        src: ['dist']
+      }
+    },
+    copy: {
+      main: {
+        expand: true,
+        cwd: 'src/',
+        src: '**',
+        dest: 'dist/',
+      },
+    },
     jshint: {
       files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
       options: {
@@ -15,9 +28,11 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('build', ['clean', 'copy']);
 
 };
